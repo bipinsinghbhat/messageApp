d2bba9a8a00da318add888754204ad03915a76b3
bipinsinghbhat <bipinsinghbhat1698@gmail.com>
2024-09-16 13:24:00 +0530

    Fixed tracking issue with frontend folder

---
+++ frontend/src/component/HomePage.jsx
@@ -0,0 +1,12 @@
+const HomePage=()=>{
+   return (
+      <div>
+         <p>hi hlowe</p>
+         <p>yes home</p>
+         <p>yesss</p>
+         <p>jijijiji</p>
+      </div>
+   )
+}
+
+export default HomePage
